82338efe7ce9e7fc7063f56ac139db3be8632730
seongmind <62093201+seongmind@users.noreply.github.com>
2023-05-10 13:59:44 +0900

    프로그래머스 문제풀이
    전력망을 둘로 나누기,
    모음 사전

---
+++ Algorithm/Programmers/HighScore_Kit/06_Greedy/main.swift
@@ -0,0 +1,34 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 김성민 on 2023/05/10.
+//
+// 체육복
+// 탐욕법(Greedy)
+// LV.1
+// https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=swift
+
+import Foundation
+
+func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
+    // solving...
+    
+    return 0
+}
+
+print(solution(
+    5,
+    [2, 4],
+    [1, 3, 5]
+))
+print(solution(
+    5,
+    [2, 4],
+    [3]
+))
+print(solution(
+    3,
+    [3],
+    [1]
+))
