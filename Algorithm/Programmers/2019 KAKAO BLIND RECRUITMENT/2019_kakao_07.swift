90a1dcabe38910a70d1cf04d733d87f54cc9981f
seongmind <62093201+seongmind@users.noreply.github.com>
2023-07-25 15:18:28 +0900

    프로그래머스 문제풀이
    블록 게임

---
+++ Algorithm/Programmers/2019 KAKAO BLIND RECRUITMENT/2019_kakao_07.swift
@@ -0,0 +1,92 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 김성민 on 2023/07/25.
+//
+// 블록 게임
+// 2019 KAKAO BLIND RECRUITMENT
+// Lv.3
+// https://school.programmers.co.kr/learn/courses/30/lessons/42894
+
+import Foundation
+
+func solution(_ board:[[Int]]) -> Int {
+    // case6: 시간초과
+    
+    let n = board.count
+    var board = board
+
+    // 내 위가 모두 빈칸이면 검은색 블록을 채울 수 있음
+    func canFill(_ x: Int, _ y: Int) -> Bool {
+        for i in 0..<x {
+            if board[i][y] != 0 {
+                return false
+            }
+        }
+        return true
+    }
+
+    func canDelete(_ x: Int, _ y: Int, _ h: Int, _ w: Int) -> Bool {
+        // 빈칸 2칸 + 블록 4칸
+        // 빈칸은 모두 검은 블록으로 채울 수 있어야 함
+        // 블록의 숫자는 모두 같아야 함
+        var emptyCnt = 0
+        var value = -1
+        for i in x..<x+h {
+            for j in y..<y+w {
+                if board[i][j] == 0 {
+                    emptyCnt += 1
+                    // 빈칸이 두칸 이상이면 x
+                    if emptyCnt > 2 {
+                        return false
+                    }
+                    // 채울 수 없는 빈칸이면 x
+                    if !canFill(i, j) {
+                        return false
+                    }
+                } else {
+                    if value == -1 {
+                        value = board[i][j]
+                    } else {
+                        // 다른 블록이 섞여 있으면 x
+                        if value != board[i][j] {
+                            return false
+                        }
+                    }
+                }
+            }
+        }
+
+        // 블록 삭제
+        for i in x..<x+h {
+            for j in y..<y+w {
+                board[i][j] = 0
+            }
+        }
+        return true
+    }
+
+    var ans = 0
+    while true {
+        // 삭제한 블록 갯수
+        var cnt = 0
+        for i in 0..<n {
+            for j in 0..<n {
+                // 2*3, 3*2 확인
+                if i+2 <= n && j+3 <= n && canDelete(i, j, 2, 3) {
+                    cnt += 1
+                } else if i+3 <= n && j+2 <= n && canDelete(i, j, 3, 2) {
+                    cnt += 1
+                }
+            }
+        }
+
+        if cnt == 0 { break }
+        ans += cnt
+    }
+
+    return ans
+}
+
+print(solution([[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,4,0,0,0],[0,0,0,0,0,4,4,0,0,0],[0,0,0,0,3,0,4,0,0,0],[0,0,0,2,3,0,0,0,5,5],[1,2,2,2,3,3,0,0,0,5],[1,1,1,0,0,0,0,0,0,5]]))
--- Algorithm/Programmers/2019 KAKAO BLIND RECRUITMENT/main.swift
+++ Algorithm/Programmers/2019 KAKAO BLIND RECRUITMENT/main.swift
@@ -4,5 +4,21 @@
 //
 //  Created by 김성민 on 2023/07/24.
 //
+// 매칭 점수
+// 2019 KAKAO BLIND RECRUITMENT
+// Lv.3
+// https://school.programmers.co.kr/learn/courses/30/lessons/42893
 
 import Foundation
+
+func solution(_ word:String, _ pages:[String]) -> Int {
+    // 기본점수: 검색어가 등장하는 횟수 (대소문자 무시)
+    // 외부 링크 수: 다른 외부 페이지로 연결된 링크의 개수
+    // 링크점수: (링크가 걸린 다른 웹페이지의 기본점수 / 외부 링크 수)의 총합
+    // 매칭점수: 기본점수 + 링크점수
+    
+    return 0
+}
+
+print(solution("blind", ["<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://a.com\"/>\n</head>  \n<body>\nBlind Lorem Blind ipsum dolor Blind test sit amet, consectetur adipiscing elit. \n<a href=\"https://b.com\"> Link to b </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://b.com\"/>\n</head>  \n<body>\nSuspendisse potenti. Vivamus venenatis tellus non turpis bibendum, \n<a href=\"https://a.com\"> Link to a </a>\nblind sed congue urna varius. Suspendisse feugiat nisl ligula, quis malesuada felis hendrerit ut.\n<a href=\"https://c.com\"> Link to c </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://c.com\"/>\n</head>  \n<body>\nUt condimentum urna at felis sodales rutrum. Sed dapibus cursus diam, non interdum nulla tempor nec. Phasellus rutrum enim at orci consectetu blind\n<a href=\"https://a.com\"> Link to a </a>\n</body>\n</html>"]))
+print(solution("Muzi", ["<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://careers.kakao.com/interview/list\"/>\n</head>  \n<body>\n<a href=\"https://programmers.co.kr/learn/courses/4673\"></a>#!MuziMuzi!)jayg07con&&\n\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://www.kakaocorp.com\"/>\n</head>  \n<body>\ncon%\tmuzI92apeach&2<a href=\"https://hashcode.co.kr/tos\"></a>\n\n\t^\n</body>\n</html>"]))
