4f8945a5292ee94906ba90ddcff8ae9cdb98ee6a
seongmind <62093201+seongmind@users.noreply.github.com>
2023-07-27 00:06:01 +0900

    프로그래머스 문제풀이
    가사 검색

--- Algorithm/Programmers/2020 KAKAO BLIND RECRUITMENT/main.swift
+++ Algorithm/Programmers/2020 KAKAO BLIND RECRUITMENT/main.swift
@@ -2,7 +2,18 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by 김성민 on 2023/07/25.
+//  Created by 김성민 on 2023/07/27.
 //
+// 기둥과 보 설치
+// 2020 KAKAO BLIND RECRUITMENT
+// Lv.3
+// https://school.programmers.co.kr/learn/courses/30/lessons/60061
 
 import Foundation
+
+func solution(_ n:Int, _ build_frame:[[Int]]) -> [[Int]] {
+    return []
+}
+
+print(solution(5, [[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]]))
+print(solution(5, [[0,0,0,1],[2,0,0,1],[4,0,0,1],[0,1,1,1],[1,1,1,1],[2,1,1,1],[3,1,1,1],[2,0,0,0],[1,1,1,0],[2,2,0,1]]))
