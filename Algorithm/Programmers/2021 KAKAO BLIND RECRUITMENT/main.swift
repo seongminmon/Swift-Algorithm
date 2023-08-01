e46f7bcde1a7bc0cfeebbfef121352f65b0b7224
seongmind <62093201+seongmind@users.noreply.github.com>
2023-08-01 16:44:33 +0900

    프로그래머스 문제 풀이
    신규 아이디 추천

---
+++ Algorithm/Programmers/2021 KAKAO BLIND RECRUITMENT/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 김성민 on 2023/08/01.
+//
+// 메뉴 리뉴얼
+// 2021 KAKAO BLIND RECRUITMENT
+// Lv.2
+// https://school.programmers.co.kr/learn/courses/30/lessons/72411
+
+import Foundation
+
+func solution(_ orders:[String], _ course:[Int]) -> [String] {
+    return []
+}
+
+print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4]))
+print(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2,3,5]))
+print(solution(["XYZ", "XWY", "WXA"], [2,3,4]))
