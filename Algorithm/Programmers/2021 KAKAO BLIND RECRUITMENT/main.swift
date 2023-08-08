02f8665928ac01d21126eeff93628f392cb3467f
seongmind <62093201+seongmind@users.noreply.github.com>
2023-08-08 15:28:40 +0900

    프로그래머스 문제풀이
    광고 삽입

--- Algorithm/Programmers/2021 KAKAO BLIND RECRUITMENT/main.swift
+++ Algorithm/Programmers/2021 KAKAO BLIND RECRUITMENT/main.swift
@@ -4,9 +4,5 @@
 //
 //  Created by 김성민 on 2023/08/08.
 //
-// 광고 삽입
-// 2021 KAKAO BLIND RECRUITMENT
-// Lv.3
-// https://school.programmers.co.kr/learn/courses/30/lessons/72414
 
 import Foundation
