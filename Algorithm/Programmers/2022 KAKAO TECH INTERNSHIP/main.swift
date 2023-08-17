2f7a555f2f26a04542f470a051b970e3d4809938
seongmind <62093201+seongmind@users.noreply.github.com>
2023-08-17 16:37:11 +0900

    프로그래머스 문제풀이
    두 큐 합 같게 만들기

--- Algorithm/Programmers/2022 KAKAO TECH INTERNSHIP/main.swift
+++ Algorithm/Programmers/2022 KAKAO TECH INTERNSHIP/main.swift
@@ -4,9 +4,5 @@
 //
 //  Created by 김성민 on 2023/08/17.
 //
-// 두 큐 합 같게 만들기
-// 2022 KAKAO TECH INTERNSHIP
-// Lv.2
-// https://school.programmers.co.kr/learn/courses/30/lessons/118667
 
 import Foundation
