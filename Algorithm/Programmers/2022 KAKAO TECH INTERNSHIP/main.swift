569c10e08429b742a5fef44707a662dd406e580b
seongmind <62093201+seongmind@users.noreply.github.com>
2023-08-17 15:47:42 +0900

    프로그래머스 문제풀이
    성격 유형 검사하기

--- Algorithm/Programmers/2022 KAKAO TECH INTERNSHIP/main.swift
+++ Algorithm/Programmers/2022 KAKAO TECH INTERNSHIP/main.swift
@@ -4,9 +4,9 @@
 //
 //  Created by 김성민 on 2023/08/17.
 //
-// 성격 유형 검사하기
+// 두 큐 합 같게 만들기
 // 2022 KAKAO TECH INTERNSHIP
-// Lv.1
-// https://school.programmers.co.kr/learn/courses/30/lessons/118666
+// Lv.2
+// https://school.programmers.co.kr/learn/courses/30/lessons/118667
 
 import Foundation
