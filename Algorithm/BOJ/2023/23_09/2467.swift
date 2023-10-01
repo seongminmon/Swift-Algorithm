7fc0ea9fb652e05e6074b2c025a2e947451adb66
seongmind <62093201+seongmind@users.noreply.github.com>
2023-10-01 12:41:48 +0900

    2467 : 용액

---
+++ Algorithm/BOJ/2023/23_09/2467.swift
@@ -0,0 +1,42 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by 김성민 on 2023/09/30.
+//
+// 2467 : 용액
+// https://www.acmicpc.net/problem/2467
+
+import Foundation
+
+let n = Int(readLine()!)!
+var arr = readLine()!.split(separator: " ").map { Int(String($0))! }
+
+var minDiff = Int.max
+var ans = [0,0]
+
+var start = 0
+var end = n-1
+while start < end {
+    let diff = abs(arr[start] + arr[end])
+    
+    // 갱신
+    if minDiff > diff {
+        minDiff = diff
+        ans = [arr[start], arr[end]]
+    }
+    
+    if arr[start] >= 0 && arr[end] >= 0 {
+        end -= 1
+    } else if arr[start] < 0 && arr[end] < 0 {
+        start += 1
+    } else {
+        if abs(arr[start]) > arr[end] {
+            start += 1
+        } else {
+            end -= 1
+        }
+    }
+}
+
+print(ans[0], ans[1])
