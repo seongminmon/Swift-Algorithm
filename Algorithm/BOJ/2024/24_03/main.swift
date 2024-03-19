6ad941f80c07fe53bdc2702393bd2b19e78a48ce
seongmind <62093201+seongmind@users.noreply.github.com>
2024-03-19 17:31:00 +0900

    15662 : 톱니바퀴 (2)

--- Algorithm/BOJ/2024/24_03/main.swift
+++ Algorithm/BOJ/2024/24_03/main.swift
@@ -2,10 +2,7 @@
 //  main.swift
 //  Algorithm
 //
-//  Created by 김성민 on 3/4/24.
+//  Created by 김성민 on 3/19/24.
 //
-// 15662 : 톱니바퀴 (2)
-// https://www.acmicpc.net/problem/15662
 
 import Foundation
-
